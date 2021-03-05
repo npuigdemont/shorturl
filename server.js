44816c4b83ec23b8ffec17822c5a0d23e11b2931
Glitch (tall-judicious-vinca) <none>
2021-03-05 11:13:57 +0000

    😜✋ Checkpoint ./server.js:29336336/1012

--- server.js
+++ server.js
@@ -28,4 +28,38 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 useFindAndModify: false});
 console.log(mongoose.connection.readyState);
 
-const { Schema, model }= mongoose
+var Schema = mongoose.Schema;
+var urlShortnerSchema = new Schema({
+  url: String
+});
+var urlShortner = mongoose.model("urlShortner", urlShortnerSchema);
+
+let urlExtractor = function(url) {
+  var urlSplit = url.split("https://");
+  if (urlSplit[1] == undefined) {
+    return urlSplit[0].split("/")[0];
+  } else {
+    return urlSplit[1].split("/")[0];
+  }
+};
+
+
+//post url
+app.post("/api/shorturl/new", function(req, res) {
+  var url = req.body.url;
+  var extractedUrl = urlExtractor(req.body.url);
+  dns.resolveAny(extractedUrl, (err, address) => {
+    if (err) {
+      console.log(err, address);
+      res.json({ error: "invalid URL" });
+    } else {
+      var urlRecord = new urlShortner({ url: url });
+      urlRecord.save((err, data) => {
+        if (err) res.json({ error: "invalid URL" });
+        else {
+          res.json({ original_url: url, short_url: data._id.toString() });
+        }
+      });
+    }
+  });
+});
